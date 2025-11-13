0a26aa92745d7f740b68608292deaae115618dea
Niru200374 <nirutasilwal21@gmail.com>
2025-11-13 08:21:29 +0545

    task 6

--- Task5/obj/Debug/net9.0/Task5.AssemblyInfo.cs
+++ Task5/obj/Debug/net9.0/Task5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6228e03fae6385c66307c5f6b54c5ff49e0464a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724f5d6effe34149b0b17bb50fa045dff444e05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task6
+{
+    class Program
+    {
+        public static void Main() // Fixed method name to 'Main' and made it static
+        {
+            Program program = new Program(); // Create an instance to call non-static methods
+            program.ConvertInput();
+
+            program.CheckPassword();
+        }
+        public void ConvertInput()
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered: " + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed");
+            }
+        }
+
+        public void CheckPassword()
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+            else
+            {
+                Console.WriteLine("Password satisfied the requirement");
+            }
+        }
+    }
+}
